2e834248544d5188c983d3aa824c1c31bc00b8b1
bwu <bewu@mozilla.com>
2024-04-10 15:52:36 -0400

    black

--- public_data_report/annotations/annotations.py
+++ public_data_report/annotations/annotations.py
@@ -20,18 +20,10 @@ FXHEALTH_ANNOTATIONS_FILE = "annotations_fxhealth.json"
 # Annotations that are added to every country
 DEFAULT_USAGE_ANNOTATIONS = [
     {
-      "annotation": {
-        "pct_TP": "FF57",
-        "pct_addon": "legacy addons disabled"
-      },
-      "date": "2017-11-14"
+        "annotation": {"pct_TP": "FF57", "pct_addon": "legacy addons disabled"},
+        "date": "2017-11-14",
     },
-    {
-      "annotation": {
-        "pct_addon": "data deleted (addons outage)"
-      },
-      "date": "2019-05-05"
-    }
+    {"annotation": {"pct_addon": "data deleted (addons outage)"}, "date": "2019-05-05"},
 ]
 
 
@@ -39,43 +31,45 @@ def get_fxhealth_annotations(date_to: datetime.datetime) -> str:
     """Return JSON string of annotations for Firefox versions per country."""
     client = bigquery.Client()
 
-    QUERY = f"""WITH days AS (
-                        SELECT
-                            day
-                        FROM
-                            UNNEST(
-                                GENERATE_DATE_ARRAY(
-                                    '2018-12-31',
-                                    '{date_to.date()}',
-                                    INTERVAL 7 DAY)
-                            ) AS day
-                    ),
-                    latest_version_per_day AS (
-                        SELECT
-                            day,
-                            MAX(`mozfun.norm.truncate_version`(build.target.version, "major")
-                        ) AS version
-                        FROM
-                            `moz-fx-data-shared-prod.telemetry.buildhub2`
-                        JOIN
-                            days
-                        ON
-                            DATE(build.build.date) <= day
-                        WHERE
-                            build.target.channel = 'release'
-                            AND DATE(build.build.date) >= '2018-10-31'
-                        GROUP BY
-                            day
-                    )
-                    SELECT
-                        MIN(FORMAT_DATE('%Y-%m-%d', day)) AS day,
-                        version
-                    FROM
-                        latest_version_per_day
-                    GROUP BY
-                        version
-                    ORDER BY
-                        day DESC"""
+    QUERY = f"""
+    WITH days AS (
+        SELECT
+            day
+        FROM
+            UNNEST(
+                GENERATE_DATE_ARRAY(
+                    '2018-12-31',
+                    '{date_to.date()}',
+                    INTERVAL 7 DAY)
+            ) AS day
+    ),
+    latest_version_per_day AS (
+        SELECT
+            day,
+            MAX(`mozfun.norm.truncate_version`(build.target.version, "major")
+        ) AS version
+        FROM
+            `moz-fx-data-shared-prod.telemetry.buildhub2`
+        JOIN
+            days
+        ON
+            DATE(build.build.date) <= day
+        WHERE
+            build.target.channel = 'release'
+            AND DATE(build.build.date) >= '2018-10-31'
+        GROUP BY
+            day
+    )
+    SELECT
+        MIN(FORMAT_DATE('%Y-%m-%d', day)) AS day,
+        version
+    FROM
+        latest_version_per_day
+    GROUP BY
+        version
+    ORDER BY
+        day DESC
+    """
 
     query_job = client.query(QUERY)
     rows = query_job.result()
@@ -83,19 +77,21 @@ def get_fxhealth_annotations(date_to: datetime.datetime) -> str:
     fxhealth_annotations = {country: [] for country in USER_ACITVITY_COUNTRY_LIST}
     for row in rows:
         for country in USER_ACITVITY_COUNTRY_LIST:
-            fxhealth_annotations[country].append({
-                "annotation": {
-                    "pct_latest_version": f"FF{row['version']}"
-                },
-                "date": row["day"]
-            })
+            fxhealth_annotations[country].append(
+                {
+                    "annotation": {"pct_latest_version": f"FF{row['version']}"},
+                    "date": row["day"],
+                }
+            )
 
     return json.dumps(fxhealth_annotations, indent=2)
 
 
 def get_usage_annotations() -> str:
     """Return JSON string of annotations for Firefox usage per country."""
-    usage_annotations = json.loads(pkg_resources.read_text(static, WEBUSAGE_ANNOTATIONS_FILE))
+    usage_annotations = json.loads(
+        pkg_resources.read_text(static, WEBUSAGE_ANNOTATIONS_FILE)
+    )
     for country in USER_ACITVITY_COUNTRY_LIST:
         if country not in usage_annotations:
             usage_annotations[country] = []
@@ -111,8 +107,8 @@ def get_usage_annotations() -> str:
     required=True,
     help="End date for producing version release dates",
 )
-@click.option('--output_bucket', required=True)
-@click.option('--output_prefix', required=True)
+@click.option("--output_bucket", required=True)
+@click.option("--output_prefix", required=True)
 def main(date_to, output_bucket, output_prefix):
     """Export annotations to GCS"""
 
@@ -120,7 +116,9 @@ def main(date_to, output_bucket, output_prefix):
 
     usage_annotations_json = get_usage_annotations()
 
-    hardware_annotations_json = pkg_resources.read_text(static, HARDWARE_ANNOTATIONS_FILE)
+    hardware_annotations_json = pkg_resources.read_text(
+        static, HARDWARE_ANNOTATIONS_FILE
+    )
 
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(output_bucket)
@@ -130,9 +128,14 @@ def main(date_to, output_bucket, output_prefix):
         (hardware_annotations_json, HARDWARE_ANNOTATIONS_FILE),
     ]:
         blob_static_annotation = bucket.blob(f"{output_prefix}/{filename}")
-        blob_static_annotation.upload_from_string(json_data, content_type="application/json")
-        logging.info(f"Uploaded {blob_static_annotation.size} bytes to {bucket.name}/{blob_static_annotation.name}")
+        blob_static_annotation.upload_from_string(
+            json_data, content_type="application/json"
+        )
+        logging.info(
+            f"Uploaded {blob_static_annotation.size} bytes to "
+            f"{bucket.name}/{blob_static_annotation.name}"
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- public_data_report/user_activity/user_activity.py
+++ public_data_report/user_activity/user_activity.py
@@ -11,8 +11,11 @@ logger = logging.getLogger(__name__)
 
 
 @click.command()
-@click.option("--bq_table", required=True,
-              help="Input BigQuery table containing aggregated metrics")
+@click.option(
+    "--bq_table",
+    required=True,
+    help="Input BigQuery table containing aggregated metrics",
+)
 @click.option("--gcs_bucket", required=True, help="GCS bucket for exporting data")
 @click.option("--gcs_path", required=True, help="GCS path for exporting data")
 def main(bq_table, gcs_bucket, gcs_path):
@@ -28,7 +31,7 @@ def main(bq_table, gcs_bucket, gcs_path):
         top_addons, has_addon_ratio, top_locales
         FROM `{bq_table}`
         WHERE country_name IN (
-            {", ".join(map(lambda x: f"'{x}'", USER_ACITVITY_COUNTRY_LIST))}    
+            {", ".join(map(lambda x: f"'{x}'", USER_ACITVITY_COUNTRY_LIST))}
         )
     """
 
@@ -39,39 +42,55 @@ def main(bq_table, gcs_bucket, gcs_path):
     web_usage_metrics = {}
 
     for row in rows:
-        if row.country_name not in user_activity_metrics \
-                and row.country_name not in web_usage_metrics:
+        if (
+            row.country_name not in user_activity_metrics
+            and row.country_name not in web_usage_metrics
+        ):
             user_activity_metrics[row.country_name] = []
             web_usage_metrics[row.country_name] = []
 
-        user_activity_metrics[row.country_name].append({
-            "date": row.date,
-            "metrics": {
-                "avg_intensity": float(row.intensity),
-                "MAU": row.mau * 100,
-                "avg_daily_usage(hours)": float(row.avg_hours_usage_daily),
-                "pct_new_user": float(row.new_profile_rate) * 100,
-                "pct_latest_version": float(row.latest_version_ratio) * 100
-            }
-        })
-        web_usage_metrics[row.country_name].append({
+        user_activity_metrics[row.country_name].append(
+            {
                 "date": row.date,
                 "metrics": {
-                    "locale": {l["locale"]: l["ratio"] * 100 for l in row["top_locales"]}, # noqa
-                    "top10addons": {a["addon_name"]: a["ratio"] * 100 for a in row["top_addons"]}, # noqa
-                    "pct_addon": row["has_addon_ratio"] * 100
-                }
-            })
+                    "avg_intensity": float(row.intensity),
+                    "MAU": row.mau * 100,
+                    "avg_daily_usage(hours)": float(row.avg_hours_usage_daily),
+                    "pct_new_user": float(row.new_profile_rate) * 100,
+                    "pct_latest_version": float(row.latest_version_ratio) * 100,
+                },
+            }
+        )
+        web_usage_metrics[row.country_name].append(
+            {
+                "date": row.date,
+                "metrics": {
+                    "locale": {
+                        locale["locale"]: locale["ratio"] * 100 for locale in row["top_locales"]
+                    },
+                    "top10addons": {
+                        addon["addon_name"]: addon["ratio"] * 100 for addon in row["top_addons"]
+                    },
+                    "pct_addon": row["has_addon_ratio"] * 100,
+                },
+            }
+        )
 
     # validate country list
     country_allowlist = set(USER_ACITVITY_COUNTRY_LIST)
-    missing_countries = country_allowlist - (set(web_usage_metrics.keys()) | web_usage_metrics.keys())
-    unexpected_countries = (set(web_usage_metrics.keys()) | web_usage_metrics.keys()) - country_allowlist
+    missing_countries = country_allowlist - (
+        set(web_usage_metrics.keys()) | web_usage_metrics.keys()
+    )
+    unexpected_countries = (
+        set(web_usage_metrics.keys()) | web_usage_metrics.keys()
+    ) - country_allowlist
     errors = []
     if len(missing_countries) > 0:
         errors.append(f"Expected countries missing: {missing_countries}")
     if len(unexpected_countries) > 0:
-        errors.append(f"Countries not in allowlist but included in output: {unexpected_countries}")
+        errors.append(
+            f"Countries not in allowlist but included in output: {unexpected_countries}"
+        )
     if len(errors) > 0:
         raise RuntimeError(f"Invalid countries in output: {', '.join(errors)}")
 
@@ -85,8 +104,7 @@ def main(bq_table, gcs_bucket, gcs_path):
         json.dumps(user_activity_metrics, indent=4)
         blob = bucket.blob(f"{gcs_path}/{filename}")
         blob.upload_from_string(
-            json.dumps(json_data, indent=4),
-            content_type="application/json"
+            json.dumps(json_data, indent=4), content_type="application/json"
         )
         logging.info(f"Uploaded {blob.size} bytes to {bucket.name}/{blob.name}")
 
